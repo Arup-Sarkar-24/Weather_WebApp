12ad3878ca844b82f31f719386dd1c6c7ceb5489
arup-sarkar-24 <1arupsarkar112@gmail.com>
2024-11-10 09:16:42 +0530

    weather web app created

---
+++ weatherapp/src/test/java/com/mycompany/weatherapp/WeatherappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
